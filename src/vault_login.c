3fa68b391aa96f0dc3231ba8e1e8983529841fc1
Rai Muhammad Umar <raiumar@hotmail.com>
2025-08-27 19:40:21 -0400

    Added login_user Function

--- src/vault_login.c
+++ src/vault_login.c
@@ -1,5 +1,6 @@
 #include "../include/header.h"
 
+/* Initialize New User */
 static int init_user(){
     char pwd[1024]; char hashed[crypto_pwhash_STRBYTES]; const char *path = "user.pass"; // Initialize Variables
 
@@ -13,7 +14,7 @@ static int init_user(){
 
     sodium_memzero(pwd, sizeof(pwd));
 
-    if (write_file(path, hashed, strlen(hashed)) != 1){
+    if (write_file(path, hashed, strlen(hashed)) != 1){ // Write the Hash
         sodium_memzero(hashed, sizeof(hashed));
         return -1;
     }
@@ -21,4 +22,39 @@ static int init_user(){
 
     printf("User Created\n");
     return 1;
+}
+
+/* Login User */
+int login_user(){
+    unsigned char *filebuf = NULL; const char *path = "user.pass"; size_t filelen = 0; char pwd[1024]; // Initialize Variables
+
+    if (read_file(path, &filebuf, &filelen) != 1){ // Read Password File
+        printf("No password file found!\n");
+        return -1;
+    }
+
+    filebuf = realloc(filebuf, filelen + 1);
+    if (!filebuf){
+        printf("Realloc Failed!\n");
+        return -1;
+    }
+    filebuf[filelen] = '\0';
+
+    if (prompt_password("Enter Password: ", pwd, sizeof (pwd), 0) != 1){
+        sodium_free(filebuf);
+        return -1;
+    }
+
+    int success = crypto_pwhash_str_verify(filebuf, pwd, strlen(pwd)); // Check if Password Matches
+
+    sodium_memzero(pwd, sideof(pwd));
+    sodium_free(filebuf);
+
+    if (success == 0){
+        printf("Login Success\n");
+        return 1;
+    } else {
+        printf("Login Failed\n");
+        return -1;
+    }
 }
