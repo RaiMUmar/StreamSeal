c4ddb31e0931de4bd241b29dfc393b783f9eae2f
Rai Muhammad Umar <raiumar@hotmail.com>
2025-08-28 17:49:54 -0400

    Added print_hex Function

---
+++ src/vault_print_hex.c
@@ -0,0 +1,9 @@
+#include "../include/header.h"
+
+void print_hex (const char *label, const unsigned char *buf, size_t len) {
+    print("%s", label);
+    for (size_t i = 0; i < len; i++){
+        printf("%02x", buf[i]);
+    }
+    printf("\n");
+}
