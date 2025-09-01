5856f50bbeae1ede2a4462600e2964ac4647479e
Rai Muhammad Umar <raiumar@hotmail.com>
2025-09-01 13:41:59 -0400

    Added encrupt_file Function

--- include/header.h
+++ include/header.h
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include <sodium.h>
+#include <stdlib.h> 
+#include <stdint.h> 
+#include <string.h>
+
+static const uint8_t MAGIC[6] = { 'S','I','M','P','L','1' };
+
+typedef struct {
+    uint8_t  magic[6];
+    unsigned char salt[16];
+    unsigned char nonce[12];
+} simple_hdr_t;
