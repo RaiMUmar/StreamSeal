e56997fd75cfcba1ad9eef3364b18cc592c05643
Rai Muhammad Umar <raiumar@hotmail.com>
2025-09-06 15:01:59 -0400

    Added Function Prototypes

--- include/header.h
+++ include/header.h
@@ -11,3 +11,15 @@ typedef struct {
     unsigned char salt[16];
     unsigned char nonce[12];
 } simple_hdr_t;
+
+
+int main(int argc, char **argv);
+int decrypt_file (const char *in_path, const char *out_path);
+static int encrypt_file (const char *in_path, const char *out_path);
+int read_file (const char*path, unsigned char **buff, size_t *len);
+int write_file(const char *path, const unsigned char *buf, size_t len);
+int prompt_password(const char *label, char *out, size_t outsz, int confirm);
+static int init_user();
+int login_user();
+void print_hex (const char *label, const unsigned char *buf, size_t len);
+static void usage(const char *prog);
